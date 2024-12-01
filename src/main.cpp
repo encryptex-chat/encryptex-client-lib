321711e077a5f4895a46104d5607bdd55d206c08
AkarTV <AkarTV@github.com>
2024-12-01 22:55:52 +0300

    Fix clang-format and add error code checking

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ using boost::asio::ip::tcp;
 
 constexpr bool k_async_write{true};
 
-void write_handler(const boost::system::error_code& error, std::size_t bytes_transferred)
+void write_handler(const boost::system::error_code& error, const std::size_t bytes_transferred)
 {
     std::cout << error.what() << ", bytes transfered: " << bytes_transferred << std::endl;
 }
@@ -16,14 +16,25 @@ int main()
 {
     try
     {
+        boost::system::error_code ec;
         boost::asio::io_context io_context;
 
         tcp::resolver resolver(io_context);
         tcp::resolver::results_type endpoints = resolver.resolve(
-            etex::common::k_server_addr, std::to_string(etex::common::k_server_port));
+            etex::common::k_server_addr, std::to_string(etex::common::k_server_port), ec);
+        if (ec)
+        {
+            std::cerr << "Error: " << ec.what() << std::endl;
+            return ec.value();
+        }
 
         tcp::socket socket(io_context);
-        boost::asio::connect(socket, endpoints);
+        boost::asio::connect(socket, endpoints, ec);
+        if (ec)
+        {
+            std::cerr << "Error: " << ec.what() << std::endl;
+            return ec.value();
+        }
 
         const std::string msg{"Hello, bitches!\n"};
 
@@ -31,13 +42,22 @@ int main()
         {
             socket.async_write_some(boost::asio::buffer(msg), &write_handler);
             io_context.run();
-        } else
-        {
-            const auto bytes_written = socket.write_some(boost::asio::buffer(msg));
-            std::cout << "Written " << bytes_written << " bytes" << std::endl;
         }
-
-    } catch (std::exception& e)
+        else
+        {
+            const auto bytes_written = socket.write_some(boost::asio::buffer(msg), ec);
+            if (ec)
+            {
+                std::cerr << "Error: " << ec.what() << std::endl;
+                return ec.value();
+            }
+            else
+            {
+                std::cout << "Sent " << bytes_written << " bytes" << std::endl;
+            }
+        }
+    }
+    catch (std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
